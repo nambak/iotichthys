388199638415956d26e864b1a3bcbb4eec23917d
Alex Nam <nambak80@gmail.com>
2025-05-31 17:02:56 +0900

    로고 이미지 변경

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -3,7 +3,6 @@
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
-
     <form wire:submit="login" class="flex flex-col gap-6">
         <!-- Email Address -->
         <flux:input
