9d8d7a0e901aa50d08beb023b2afd51cf4dbd0db
Alex Nam <nambak80@gmail.com>
2025-06-13 14:43:40 +0900

    조직 등록에 필요한 폼 필드 추가

--- resources/views/livewire/organization/create-modal.blade.php
+++ resources/views/livewire/organization/create-modal.blade.php
@@ -3,8 +3,11 @@
         <div>
             <flux:heading size="lg">{{ __('새 조직 추가') }}</flux:heading>
         </div>
-        <flux:input label="Name" placeholder="Your name" />
-        <flux:input label="Date of birth" type="date" />
+        <flux:input label="조직 이름" placeholder="조직 이름을 입력해 주세요" />
+        <flux:input label="대표자 명" placeholder="대표자 명을 입력해 주세요" />
+        <flux:input label="사업자 번호" placeholder="-을 제외한 사업자 번호를 입력해 주세요" />
+        <flux:input label="사업장 주소" placeholder="사업장 주소를 입력해 주세요" />
+        <flux:input label="사업자 전화번호" placeholder="-를 제외한 대표 전화 번호를 입력해 주세요 " />
         <div class="flex">
             <flux:spacer />
             <flux:button type="submit" variant="primary">Save changes</flux:button>
