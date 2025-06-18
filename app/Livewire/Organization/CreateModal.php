b477c4ba1cda455f7366a03969a03b8edee02680
Alex Nam <nambak80@gmail.com>
2025-06-18 17:18:48 +0900

    - 검증 오류 메시지 추가 - 검증 오류 Flux 요소 스타일 수정
    Signed-off-by: Alex Nam <nambak80@gmail.com>

--- app/Livewire/Organization/CreateModal.php
+++ app/Livewire/Organization/CreateModal.php
@@ -3,23 +3,30 @@
 namespace App\Livewire\Organization;
 
 use App\Models\Organization;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateModal extends Component
 {
+    #[Validate('required', message: '조직이름을 입력해 주세요')]
+    #[Validate('min:1', message: '최소 1글자 이상 입력해 주세요')]
     public $name;
-    public $owner;
-    public $businessRegisterNumber;
-    public $address;
-    public $phoneNumber;
 
-    protected $rules = [
-        'name'                   => 'required|min:1',
-        'owner'                  => 'required|min:2',
-        'businessRegisterNumber' => 'required|min:10',
-        'address'                => 'required|min:10',
-        'phoneNumber'            => 'required|min:9',
-    ];
+    #[Validate('required', message: '대표자명을 입력해 주세요')]
+    #[Validate('min:2', message: '최소 2글자 이상 입력해 주세요')]
+    public $owner;
+
+    #[Validate('required', message: '사업자 번호를 입력해 주세요')]
+    #[Validate('min:10', message: '10자리 사업자 번호를 입력해 주세요')]
+    public $businessRegisterNumber;
+
+    #[Validate('required', message: '사업장 주소를 입력해 주세요')]
+    #[Validate('min:10', message: '주소를 자세히 입력해 주세요')]
+    public $address;
+
+    #[Validate('required', message: '사업자 전화번호를 입력해 주세요')]
+    #[Validate('min:9', message: '전화번호를 다시 확인해 주세요')]
+    public $phoneNumber;
 
     /**
      * 조직 생성 모달
@@ -41,9 +48,6 @@ class CreateModal extends Component
      */
     public function save()
     {
-        // TODO: 권한 체크 - Organization 생성 정책 확인
-        // $this->authorize('create', Organization::class);
-
         $validatedData = $this->validate();
 
         $organization = new Organization();
