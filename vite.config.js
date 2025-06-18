3081c9f73f2b6d25f249b5cbe058a29f605f8555
Alex Nam <nambak80@gmail.com>
2025-06-18 15:26:51 +0900

    vite 포트 번호를 하드코딩하기보다는 환경변수로 노출ˆ
    Signed-off-by: Alex Nam <nambak80@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        port: 5174,
+        // .env 파일 또는 쉘 변수에 VITE_DEV_PORT가 정의되어 있으면 그것을, 없으면 기본값 5174
+        port: Number(process.env.VITE_DEV_PORT) || 5174,
         cors: true,
     },
 });
